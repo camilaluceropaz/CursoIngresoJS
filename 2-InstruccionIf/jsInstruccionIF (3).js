96140b03f0ce9b7e41a4c87d798cf7b622b61bf2
alumno <alumno@utnfra.com.ar>
2020-02-10 19:35:00 -0300

    instruccionIf 123

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,12 @@
 function mostrar()
 {
 //tomo la edad  
+var edad = document.getElementById("edad").value;
 
+if(edad >= 18) {
+    alert("es mayor");
+} else {
+    alert("es menor");
+}
 
 }//FIN DE LA FUNCIÓN
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,73 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+var precioUno
+var precioDos
+var PrecioTres
+var resultado
+
 function Sumar () 
 {
+    
+    
+
+    precioUno = document.getElementById("PrecioUno").value;
+    precioDos = document.getElementById("PrecioDos").value;
+    precioTres = document.getElementById("PrecioTres").value;
+    
+    precioUno = parseInt(precioUno);
+    precioDos = parseInt(precioDos);
+    precioTres = parseInt(precioTres);
+
+    
+    resultado = precioUno + precioDos + precioTres;
+
+     alert(resultado);
+     console.log(resultado);
+
+
+    }
 	
-}
+
 function Promedio () 
 {
 	
+    precioUno = document.getElementById("PrecioUno").value;
+    precioDos = document.getElementById("PrecioDos").value;
+    precioTres = document.getElementById("PrecioTres").value;
+    
+    precioUno = parseInt(precioUno);
+    precioDos = parseInt(precioDos);
+    precioTres = parseInt(precioTres);
+
+    
+    resultado = (precioUno + precioDos + precioTres) / 3;
+
+     alert(resultado);
+     console.log(resultado);
+
 }
 function PrecioFinal () 
 {
-	
+    var IVA
+
+    precioUno = document.getElementById("PrecioUno").value;
+    precioDos = document.getElementById("PrecioDos").value;
+    precioTres = document.getElementById("PrecioTres").value;
+    
+    precioUno = parseInt(precioUno);
+    precioDos = parseInt(precioDos);
+    precioTres = parseInt(precioTres);
+
+    IVA = parseInt(IVA);
+    resultado = (precioUno + precioDos + precioTres)
+    IVA = resultado * 0.21 
+    resultado = resultado + IVA
+
+     alert(resultado);
+     console.log(resultado);
+
+ 
+    
+    
 }
