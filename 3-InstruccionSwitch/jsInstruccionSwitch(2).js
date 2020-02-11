57d039409e9b2819256005c4d8ebf0ec83075c8d
alumno <alumno@utnfra.com.ar>
2020-02-11 20:38:25 -0300

    switch2

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,7 +3,27 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch(mesDelAño){
+ case "Julio":
+    alert("Abritarte");
+    break;
+case "Agosto":
+    alert("Abritarte");
+    break;
+case "Enero":
+case "Febrero":
+case "Marzo":
+case "Abril":
+case "Mayo":
+case "Junio":
+     alert("falta para el invierno");
+     break;
+     
+
+     
+
+
+}
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -5,7 +5,25 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+switch(mesDelAño){
+case("Febrero"):
+    alert("tiene 28 días");
+    break;
+case "Enero":
+case "Marzo":
+case "Mayo":
+case "Julio":
+case "Agoto":
+case "Octubre":
+case "Diciembre":
+alert("tiene 31 días")
+break;
+default:
+alert("tiene 30 dias")
+
+
+
+}	
 
 
 
