4ba14aa87c5b462b8ac6e43fcf324e8959d745d0
alumno <alumno@utnfra.com.ar>
2020-02-10 21:14:54 -0300

    if4

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,7 +2,14 @@ function mostrar()
 {
 //tomo la edad  
 
-	
+    var edad
+    var estado
+    edad = document.getElementById(edad).value;
+    estado = document.getElementById(estadoCivil).value;
+
+    if(edad < 18 && estado !== soltero ){
+        alert("es muy peueño para no ser soltero")
+    }
 
 
 }//FIN DE LA FUNCIÓN
