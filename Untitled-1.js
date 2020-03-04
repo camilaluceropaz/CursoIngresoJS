f752d2e9024413383eff11393279d46ac33fb0cd
alumno <alumno@utnfra.com.ar>
2020-03-03 21:25:48 -0300

    ultimomartes2

--- Untitled-1.js
+++ Untitled-1.js
@@ -6,13 +6,15 @@
  var respuesta;
  var contFemenino;
  var contMasculino;
- var pasajeUrbano 
- var pasajeNacional
- var pasajeInternacional
-
+ var pasajeUrbano;
+ var pasajeNacional;
+ var pasajeInternacional;
 
  while(respuesta== true){
-     
+     edad= prompt("ingrese su edad");
+     sexo= prompt("ingrese f o m");
+     paisaje = prompt("ingrese si su pasaje es urbano, nacional o internacional");
+
 
  }
 while(edad< 18){
@@ -37,6 +39,36 @@ while(sexo != "f"|| sexo != "m"){
     contMasculino++;
 
 }
-while(paisaje== "urbano"|| paisaje== "nacional"|| paisaje== "internacional" ){
-     paisaje = prompt("ingrese si su pasaje es urbano, nacional o internacional ")
+while(paisaje != "urbano"|| "nacional"|| "internacional" ){
+
+    pasajeUrbano++;
+
+} if(paisaje== "nacional"){
+    pasajeNacional++;
+
+}else(paisaje== "internacional"){
+    pasajeInternacional++;
+
 }
+/*
+var repuesta
+do{
+    do{
+        nombre = prompt("ingresar nombre");
+    }while(nombre ==null || nombre== "");
+    do{
+        var edad = prompt("ingrese su eddad");
+        edad = parseInt(edad);
+    }while(isNaN(edad)|| edad< 18);
+    do{
+        var sexo= prompt("ingrese sexo")
+    }while(sexo!= "m" && sexo != "f");
+    do{
+        var pasaje = prompt("ingrese su pasaje");
+
+    }while(psaje== "urbano" || pasaje =="nacional" || pasaje== "internaconal");
+    
+
+respuesta = comfilm("desea continuar");
+}while(repuesta);
+*/
